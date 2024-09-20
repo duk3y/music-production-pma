dfea56ac202a2e8bad34ed0b36cad0b25b396ea8
Gabi Turriago-Lopez <hsk3xb@virginia.edu>
2024-09-20 19:04:51 -0400

    updated readme and updated it so view was v basic and visible from homepage

--- users/views.py
+++ users/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the users index.")
+    return HttpResponse("Welcome to the homepage!")
