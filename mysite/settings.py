5dc2577ecd8ad85cb46d0fdac7d0574100aa9f20
Gabi Turriago-Lopez <hsk3xb@virginia.edu>
2024-09-19 22:52:04 -0400

    add line to use whitenoise to handle static files instead of default configs with Django's runserver to ensure consistency

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
