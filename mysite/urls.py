c3e6128b7c703bb6316181603a4424755fee888a
Duke Nguyen <nguyendukee@gmail.com>
2024-10-01 17:38:30 -0400

    Created login page at /login/ (made templates folder + views.py for this)

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import CustomLoginView
 
 urlpatterns = [
     path("", include("users.urls")),
     path("admin/", admin.site.urls),
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
