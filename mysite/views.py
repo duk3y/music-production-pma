d5268fb24ec38a19fbba922e8f76664a844e7259
Duke Nguyen <nguyendukee@gmail.com>
2024-10-01 18:07:44 -0400

    Created views for common users and PMA admins in views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,18 @@
 from django.contrib.auth.views import LoginView
+from django.shortcuts import render
+from django.views import View
 
 class CustomLoginView(LoginView):
     template_name = 'login.html'
+
+class CommonDefaultView(View):
+    template_name = 'commondefault.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class PMAAdminDefaultView(View):
+    template_name = 'pmaadmindefault.html'
+
+    def get(self, request):
+        return render(request, self.template_name)    
