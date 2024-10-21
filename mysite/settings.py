babc797f78a37d9afa323ba6301c2228e380f372
gabiturriago <gabriela.turriago-lopez@nytimes.com>
2024-10-20 20:29:50 -0400

    remove acl none in settings (#31)

--- mysite/settings.py
+++ mysite/settings.py
@@ -165,7 +165,6 @@ if os.getenv('ENV') == 'production':
     MEDIA_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/media/'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    AWS_DEFAULT_ACL = None
     AWS_S3_FILE_OVERWRITE = False
 
 else:
