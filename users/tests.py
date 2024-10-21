cf2e82e7e34da7f54348d8ed6f4816aae8748696
Lanah Pheng <lanahpng@Lanahs-MacBook-Air.local>
2024-10-20 22:36:15 -0400

    added same testcase for join_project

--- users/tests.py
+++ users/tests.py
@@ -28,3 +28,28 @@ class GoogleOAuthRedirectionTestCase(TestCase):
 
         # Check if the user is redirected to the Google OAuth login page
         self.assertRedirects(response, '/accounts/google/login/?next=/create-project/')
+
+@override_settings(SITE_ID=1)  # Force SITE_ID to 1 during the test
+class GoogleOAuthRedirectionTestCase(TestCase):
+
+    def setUp(self):
+        # Create a Site object to associate the SocialApp with
+        site, created = Site.objects.update_or_create(
+            id=1, defaults={'domain': '127.0.0.1', 'name': 'localhost'}
+        )
+
+        # Create a SocialApp object for Google OAuth and associate it with the Site
+        google_app = SocialApp.objects.create(
+            provider='google',
+            name='Google',
+            client_id='fake-client-id',
+            secret='fake-secret-key'
+        )
+        google_app.sites.add(site)
+
+    def test_oauth_redirection(self):
+        # Access the project creation page without being logged in
+        response = self.client.get(reverse('join_project'))
+
+        # Check if the user is redirected to the Google OAuth login page
+        self.assertRedirects(response, '/accounts/google/login/?next=/join-project/')
