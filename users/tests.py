87b96c4c24c9680540ba9b84ff9b3a5251be5c75
Lanah Pheng <lanahpng@Lanahs-MacBook-Air.local>
2024-10-20 21:35:09 -0400

    basic github cl setup

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from users.models import Project  
 
-# Create your tests here.
+class GoogleOAuthRedirectionTestCase(TestCase):
+    def test_oauth_redirection(self):
+        # access the project creation page without being logged in
+        response = self.client.get(reverse('create_project'))
+        
+        # ensure user is redirected to Google OAuth login
+        self.assertRedirects(response, reverse('socialaccount_login', args=['google']))
