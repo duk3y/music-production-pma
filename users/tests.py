33e1c9130de2c44f035d39edf5442d7c393891af
Lanah Pheng <lanahpng@Lanahs-MacBook-Air.local>
2024-10-20 22:42:28 -0400

    got rid of join_project test case bc it was repetitive, added test case to see if googleoauth was sucessful

--- users/tests.py
+++ users/tests.py
@@ -29,16 +29,12 @@ class GoogleOAuthRedirectionTestCase(TestCase):
         # Check if the user is redirected to the Google OAuth login page
         self.assertRedirects(response, '/accounts/google/login/?next=/create-project/')
 
-@override_settings(SITE_ID=1)  # Force SITE_ID to 1 during the test
-class GoogleOAuthRedirectionTestCase(TestCase):
+class GoogleOAuthLoginSuccessTestCase(TestCase):
 
     def setUp(self):
-        # Create a Site object to associate the SocialApp with
         site, created = Site.objects.update_or_create(
             id=1, defaults={'domain': '127.0.0.1', 'name': 'localhost'}
         )
-
-        # Create a SocialApp object for Google OAuth and associate it with the Site
         google_app = SocialApp.objects.create(
             provider='google',
             name='Google',
@@ -47,9 +43,13 @@ class GoogleOAuthRedirectionTestCase(TestCase):
         )
         google_app.sites.add(site)
 
-    def test_oauth_redirection(self):
-        # Access the project creation page without being logged in
-        response = self.client.get(reverse('join_project'))
+    def test_successful_login_redirect(self):
+        # Simulate a successful login
+        self.client.login(username='testuser', password='password')
 
-        # Check if the user is redirected to the Google OAuth login page
-        self.assertRedirects(response, '/accounts/google/login/?next=/join-project/')
+        # Access the create project page after login
+        response = self.client.get(reverse('create_project'))
+
+        # Ensure that the response is successful (HTTP 302)
+        # 302 means logged into a page successfully (with redirect)
+        self.assertEqual(response.status_code, 302)
