21c5ef914bc9680bf7c32e933366b073274ca293
gabiturriago <gabriela.turriago-lopez@nytimes.com>
2024-10-20 22:47:23 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -185,7 +185,7 @@ else:
     STATIC_ROOT = BASE_DIR / "staticfiles"
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
