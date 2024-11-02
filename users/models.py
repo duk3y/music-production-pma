ea240b3658bce1dbeaa619fac48dc31530c520f5
eddyc-j <edward.cho.jung@gmail.com>
2024-11-02 19:27:15 -0400

    displays uploaded audio files

--- users/models.py
+++ users/models.py
@@ -13,7 +13,7 @@ class Project(models.Model):
 
 class ProjectFiles(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='uploads/')
+    file = models.FileField(upload_to='')
     uploaded_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     audio_file = models.FileField(upload_to='uploads/')
