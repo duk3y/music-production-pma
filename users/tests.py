25d51b7f74b09ceb1a83fda43827fb34aa0cc023
lanah-png <starryskybreaker@gmail.com>
2024-11-17 18:30:54 -0500

    fix logo.png static error

--- users/tests.py
+++ users/tests.py
@@ -24,12 +24,6 @@ class GoogleOAuthRedirectionTestCase(TestCase):
         # Create necessary directories if they don't exist
         os.makedirs(os.path.join(settings.BASE_DIR, 'static'), exist_ok=True)
         os.makedirs(os.path.join(settings.BASE_DIR, 'staticfiles'), exist_ok=True)
-        
-        # Create a dummy logo.png if it doesn't exist
-        logo_path = os.path.join(settings.BASE_DIR, 'static', 'logo.png')
-        if not os.path.exists(logo_path):
-            with open(logo_path, 'wb') as f:
-                f.write(b'dummy content')
 
     def setUp(self):
         # Create a Site object to associate the SocialApp with
