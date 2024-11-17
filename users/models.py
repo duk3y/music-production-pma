75f27c62b6094bbc924abd7e84e4cd0c3a1ac44e
gabiturriago <gabriela.turriago-lopez@nytimes.com>
2024-11-17 12:33:51 -0500

    Revert "Working chat version" (#45)

--- users/models.py
+++ users/models.py
@@ -32,7 +32,6 @@ class Comment(models.Model):
     text = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Optional: user who added the comment
 
-
     def __str__(self):
         return f"{self.project.name} - {self.timestamp}s: {self.text[:20]}"
 
