b063bb215131778a91be03d7fda8b57ba2af564a
Duke Nguyen <nguyendukee@gmail.com>
2024-11-18 18:30:19 -0500

    Reapply "Merge branch 'main' into public-projects-page"
    This reverts commit 878c5e9667b50375c78a9b05367e04c8eddd94f4.

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,13 +55,15 @@ INSTALLED_APPS = [
     'users',
     'storages',
     'mysite',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         "SCOPE":[
             'profile',
-            'email'
+            'email',
         ],
         "AUTH_PARAMS": {"access_type": "online"}
     }
@@ -97,6 +99,9 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5" 
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
@@ -181,6 +186,9 @@ else:
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/5.1/howto/static-files/
     # SHERRIFF: Added the static_root variable here to fix an erorr with static files not being found
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
     STATIC_URL = '/static/'
     STATIC_ROOT = BASE_DIR / "staticfiles"
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
