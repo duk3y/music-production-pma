29dcfe1183c8c8f3806eabf93764c2b86ebb0650
gabiturriago <gabiturriago@hotmail.com>
2025-01-02 15:45:41 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pgll1_1yhzl%*^vhpcj%ov!xdxb^vkr43@mkf)_p($)o^v2#r*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'music-production-pm-app-da1846f20d32.herokuapp.com']
 
